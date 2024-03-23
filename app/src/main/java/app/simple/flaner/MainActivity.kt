7b368ca369ec0b255966e5a13daa36adb0f7b6ad
Hamza417 <HamzaRizwan243@gmail.com>
2024-03-24 02:11:31 +0530

    Updated dependencies

--- app/src/main/java/app/simple/flaner/MainActivity.kt
+++ app/src/main/java/app/simple/flaner/MainActivity.kt
@@ -1,16 +1,15 @@
 package app.simple.flaner
 
 import android.os.Bundle
-import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
 import app.simple.flaner.adapter.PagerAdapter
 import app.simple.flaner.extension.activities.BaseActivity
 import app.simple.flaner.fragments.Knob
 import app.simple.flaner.fragments.Switch
-import app.simple.flaner.utils.NullSafety.isNull
 
 class MainActivity : BaseActivity() {
 
-    private lateinit var viewPager: ViewPager
+    private lateinit var viewPager: ViewPager2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +17,7 @@ class MainActivity : BaseActivity() {
 
         viewPager = findViewById(R.id.app_container)
 
-        val pagerAdapter = PagerAdapter(supportFragmentManager, 0)
+        val pagerAdapter = PagerAdapter(this)
 
         pagerAdapter.addFragment(Switch.newInstance())
         pagerAdapter.addFragment(Knob.newInstance())
--- app/src/main/java/app/simple/flaner/adapter/PagerAdapter.kt
+++ app/src/main/java/app/simple/flaner/adapter/PagerAdapter.kt
@@ -1,18 +1,18 @@
 package app.simple.flaner.adapter
 
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class PagerAdapter(fragmentManager: FragmentManager, int: Int) : FragmentStatePagerAdapter(fragmentManager, int) {
+class PagerAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
 
     private val fragments: ArrayList<Fragment> = ArrayList()
 
-    override fun getCount(): Int {
+    override fun getItemCount(): Int {
         return fragments.size
     }
 
-    override fun getItem(position: Int): Fragment {
+    override fun createFragment(position: Int): Fragment {
         return fragments[position]
     }
 
